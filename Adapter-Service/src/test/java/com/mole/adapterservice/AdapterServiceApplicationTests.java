79571cdcbac479df5b4eb7622bece885fb618c4e
임준희 <yim3370@gmail.com>
2025-04-16 16:23:41 +0900

    Init

---
+++ Adapter-Service/src/test/java/com/mole/adapterservice/AdapterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mole.adapterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdapterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
